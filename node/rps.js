a3fd5f57d4d1215167cf483cb4207d8ecd24fd65
Zachary Flower <zach@ninjaninja.net>
2015-09-11 16:41:59 -0600

    some comments, because good

--- node/rps.js
+++ node/rps.js
@@ -1,4 +1,3 @@
-// play until someome wins
 process.stdin.resume();
 process.stdin.setEncoding('utf8');
 
@@ -11,12 +10,16 @@ var choices = {
 
 process.stdout.write("Your Move: ");
 process.stdin.on('data', function (input) {
-  // http://stackoverflow.com/questions/2532218/pick-random-property-from-a-javascript-object
+  // calculate computer move
+  // source; http://stackoverflow.com/questions/2532218/pick-random-property-from-a-javascript-object
   var computer_move = Object.keys(choices)[ Object.keys(choices).length * Math.random() << 0];
+
+  // get player move, must be valid
   var human_move = input.trim();
 
   if ( typeof choices[human_move] !== 'undefined' ) {
     if ( human_move == computer_move ) {
+      // play until someome wins
       console.log("Tie, Replay!");
     } else if ( choices[human_move] == computer_move ) {
       console.log("Human Wins!");
