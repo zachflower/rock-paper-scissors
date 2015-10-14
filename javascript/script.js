3d4b4591f28bdf0e429ae91bda5babe853bfe182
Zachary Flower <zach@ninjaninja.net>
2015-10-14 13:16:09 -0600

    some javascript cleanup

--- javascript/script.js
+++ javascript/script.js
@@ -6,12 +6,13 @@ var choices = {
 };
 
 var notifier = document.getElementById('notifier');
+var input = document.getElementById('human-move');
 
 function submitMove() {
   // calculate computer move
   // source; http://stackoverflow.com/questions/2532218/pick-random-property-from-a-javascript-object
   var computerMove = Object.keys(choices)[ Object.keys(choices).length * Math.random() << 0];
-  var humanMove = document.getElementById('human-move').value.toLowerCase().trim();
+  var humanMove = input.value.toLowerCase().trim();
 
   if ( typeof choices[humanMove] !== 'undefined' ) {
     if ( humanMove == computerMove ) {
@@ -22,5 +23,12 @@ function submitMove() {
     } else if ( choices[computerMove] == humanMove ) {
       notifier.innerText = "Computer Wins!";
     }
+  } else {
+    notifier.innerText = '';
   }
+
+  input.value = '';
+  input.focus();
+
+  return false;
 }
