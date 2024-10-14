e86e5c7ee85ba7b47a971d568737f95870342a59
Cholmes303 <cj@jasonholmes.net>
2024-10-14 14:58:23 -0600

    Fixed outputs

--- python/rps.py
+++ python/rps.py
@@ -4,30 +4,30 @@ def rock_paper_scissors():
     turn_options = ['rock', 'paper', 'scissors']
 
     while True:
-        
-        player_turn = input("your move:") 
+        player_turn = input("Your Move:") 
 
         opponent_turn = random.choice(turn_options)
+
         if player_turn in turn_options:
                 print("computer chose:", opponent_turn)
             
                 if player_turn == opponent_turn:
-                    print("tie, replay!")
+                    print("Tie, Replay!")
             
                 elif player_turn == 'paper' and opponent_turn == 'rock':
-                    print("you win!")
+                    print("You Win!")
                     break
                 
                 elif player_turn == 'scissors' and opponent_turn == 'paper':
-                    print("you win!")
+                    print("You Win!")
                     break
                 
                 elif player_turn == 'rock' and opponent_turn == 'scissors':
-                    print("you win!")
+                    print("You Win!")
                     break
                 
                 else:
-                    print("computer wins!")
+                    print("Computer Wins!")
                     break
 
 if __name__ == '__main__':
